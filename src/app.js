6e12936d70e6c7661eaa798361e91ba6960e7415
Manh Duc <ducmanh200555@gmail.com>
2026-04-10 10:08:18 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import express from "express";
 import routerUse from "./routers/user.route.js";
 import routerAuth from "./routers/auth.route.js";
 import routerClub from "./routers/club.route.js";
+import cors from "cors";
+
 const app = express();
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  }),
+);
 app.use(express.json());
-app.use("/user", routerUse);
-app.use("/auth", routerAuth);
-app.use("/club", routerClub);
+
+app.use("/student-portal/user", routerUse);
+app.use("/student-portal/auth", routerAuth);
+app.use("/student-portal/club", routerClub);
 export default app;
