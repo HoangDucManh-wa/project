48334b1f7016986cb4595efc61280c7f65bbde8d
Manh Duc <ducmanh200555@gmail.com>
2026-04-22 07:49:19 +0700

    upgrade url

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(
     origin: "http://127.0.0.1:5500",
   }),
 );
-
+const baseURL = "/api/student-portal";
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/student-portal/user", routerUse);
-app.use("/student-portal/auth", routerAuth);
-app.use("/student-portal/club", routerClub);
+app.use(`${baseURL}/user`, routerUse);
+app.use(`${baseURL}/auth`, routerAuth);
+app.use(`${baseURL}/club`, routerClub);
 export default app;
